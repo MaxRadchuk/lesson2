55969be254b97bc65b986d303f120d9bd5a40a65
mradchuk <visbaden777@gmail.com>
2026-02-23 15:43:17 +0200

    docker-compose-wordpress_1

---
+++ docker-go/main.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+func main () {
+http.HandleFunc ("/", helloWorld)
+http.ListenAndServe(":8082", nil)
+}
+func helloWorld (w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf (w,"Hello from Go in docker")
+}
